cc3daa3ba8aa2c372bf527a5c85516e779728c2a
potrec <73169698+potrec@users.noreply.github.com>
2022-11-16 19:29:44 +0100

    Lab7

---
+++ Lab6/main.cpp
@@ -0,0 +1,6 @@
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/member.hpp>
+#include <iostream>
+using namespace boost::multi_index;
+using namespace std;
