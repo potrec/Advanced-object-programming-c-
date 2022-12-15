029d3db6058f2074f89489d67188be9a9245892a
potrec <potrec123@wp.pl>
2022-12-15 12:42:47 +0100

    Lab12 added

--- Lab12/cmake-build-debug/cmake_install.cmake
+++ Lab12/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/123lu/OneDrive/Pulpit/Pollub/Aplikacje Internetowe/Advanced-object-programming-c-/Lab12
+# Install script for directory: C:/Users/123lu/OneDrive/Pulpit/Pollub/c++/Advanced-object-programming-c-/Lab12
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/123lu/OneDrive/Pulpit/Pollub/Aplikacje Internetowe/Advanced-object-programming-c-/Lab12/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/123lu/OneDrive/Pulpit/Pollub/c++/Advanced-object-programming-c-/Lab12/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
